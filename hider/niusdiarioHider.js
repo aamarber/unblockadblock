677f8f5c6f5826d8976efb6782ff8fced8009340
Aarón Martin <aamarber@gmail.com>
2020-10-12 10:21:27 +0200

    Minor refactor and adding niusdiario

---
+++ hider/niusdiarioHider.js
@@ -0,0 +1,7 @@
+(function hide() {
+    const popupContainer = document.getElementsByClassName('adsInfo__container-UiYg')[0];
+
+    if (popupContainer) {
+        popupContainer.setAttribute('style','position: unset !important');
+    }
+})();
--- popup.js
+++ popup.js
@@ -2,28 +2,24 @@
     function main() {
 
         const initHiderButton = function () {
-            const hideButton = document.getElementById('hide');
-
             const scriptMapper = getScriptMapper();
 
-            hideButton.onclick = function () {
-
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.executeScript(
-                        tabs[0].id,
-                        {
-                            file: scriptMapper.getScript(new URL(tabs[0].url).hostname)
-                        });
-                });
-            };
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {
+                        file: scriptMapper.getScript(new URL(tabs[0].url).hostname)
+                    });
+            });
         }
 
         const getScriptMapper = function () {
             return {
                 scriptsMapList: [
-                    buildScriptMap(/elpais/, 'elpaishider.js'),
-                    buildScriptMap(/abc/, 'elpaishider.js'),
-                    buildScriptMap(/20minutos/, 'elpaishider.js'),
+                    buildScriptMap(/elpais/, './hider/fcabrootHider.js'),
+                    buildScriptMap(/abc/, './hider/fcabrootHider.js'),
+                    buildScriptMap(/20minutos/, './hider/fcabrootHider.js'),
+                    buildScriptMap(/niusdiario/, './hider/niusdiarioHider.js'),
                 ],
                 getScript: function (domain) {
                     const scriptMap = this.scriptsMapList.filter(x => x.matches(domain))[0];
@@ -42,8 +38,6 @@
                 domainRegex: domainRegex,
                 script: script,
                 matches: function (currentDomain) {
-                    console.error(currentDomain);
-                    console.error('HOLA');
                     return this.domainRegex.test(currentDomain);
                 }
             }
